fc4062501167ce3acd410a1de21b18d187085b72
Tamar Cohen Yogev <cohen.tamar@gmail.com>
2021-01-18 14:09:17 +0200

    before pushing to res arr

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,37 @@
 import React from 'react'
+import Item from '../models/ItemClass';
 
 class TodoItem extends React.Component{
     constructor(props) {
         super(props);
+      this.creatNewObj = this.creatNewObj.bind(this)
      
     } 
+
+    creatNewObj(){
+        const itemText = this.props.inputText;
+        const newItem = new Item ({itemText})
+        console.log(newItem.text)
+        
+    }
+
+
+    // readNewItem(){
+    //     const newItem = this.props.inputText;
+    //     const itemArr = [];
+    //     itemArr.
+
+    // }
+
+
+
     render(){
        
        
        return ( 
            <div>
-               <p>The item is: {this.props.inputText}</p>
+               <button onClick={this.creatNewObj}>Add</button>
+               <p >The item is: {this.props.inputText}</p>
            </div>
 
        )
